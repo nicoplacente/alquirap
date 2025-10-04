80078022685e430e2d3785fbf1ed6fa44d8ef10a
nicoplacente <nicolasplacente@gmail.com>
2025-10-04 03:16:13 -0300

    almost finished

---
+++ src/libs/reviews-constants.js
@@ -0,0 +1,85 @@
+export const REVIEWS = [
+  {
+    name: "Carlos Gómez",
+    review:
+      "Muy buena atención y herramientas en excelente estado. Recomiendo 100%.",
+    stars: 5,
+  },
+  {
+    name: "Laura Fernández",
+    review:
+      "Alquilé una hormigonera y funcionó perfecto. Servicio rápido y confiable.",
+    stars: 5,
+  },
+  {
+    name: "Martín López",
+    review:
+      "Gran variedad de herramientas, buenos precios y flexibilidad en el alquiler.",
+    stars: 4,
+  },
+  {
+    name: "Sofía Martínez",
+    review:
+      "Me salvaron con una escalera a último momento, muy buena predisposición.",
+    stars: 5,
+  },
+  {
+    name: "Jorge Ramírez",
+    review:
+      "El taladro que alquilé funcionó bárbaro, aunque me costó un poco coordinar la entrega.",
+    stars: 4,
+  },
+  {
+    name: "Valentina Torres",
+    review: "Excelente servicio, puntuales y muy amables. Todo salió perfecto.",
+    stars: 5,
+  },
+  {
+    name: "Andrés Castillo",
+    review:
+      "Buenos precios y herramientas en muy buen estado. Volvería a alquilar sin dudarlo.",
+    stars: 5,
+  },
+  {
+    name: "Mariana Ruiz",
+    review:
+      "El generador funcionó muy bien, aunque hubiera preferido más opciones de pago.",
+    stars: 4,
+  },
+  {
+    name: "Federico Ponce",
+    review:
+      "Alquilé varias herramientas para una obra pequeña y me simplificaron todo. Excelente experiencia.",
+    stars: 5,
+  },
+  {
+    name: "Gabriela Álvarez",
+    review:
+      "Muy buen trato y asesoramiento, me recomendaron justo lo que necesitaba.",
+    stars: 5,
+  },
+  {
+    name: "Natalia Herrera",
+    review:
+      "Todo impecable, desde la reserva hasta la devolución. ¡Muy recomendable!",
+    stars: 5,
+  },
+  {
+    name: "Pablo Medina",
+    review:
+      "El servicio es bueno, aunque me hubiera gustado que tengan más variedad de herramientas eléctricas.",
+    stars: 4,
+  },
+  {
+    name: "Camila López",
+    review:
+      "Super profesionales, me explicaron cómo usar la herramienta antes de retirarla.",
+    stars: 5,
+  },
+  {
+    name: "Hernán Sánchez",
+    review:
+      "Precios accesibles y buen servicio, aunque la amoladora estaba un poco desgastada.",
+    stars: 4,
+  },
+];
---
+++ src/libs/tools-constants.js
@@ -0,0 +1,114 @@
+export const tools = [
+  {
+    id: 1,
+    image:
+      "https://imgs.search.brave.com/Co1TEgCAqooo93YJoBlE8dZY_4hDxy03obLP6eSfhCc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90b3MtcHJlbWl1/bS9jb25zdHJ1Y2Np/b24tbWV0YWxpY2Et/YW5kYW1pb3NfMjc0/NjI2LTYxMzEuanBn/P3NlbXQ9YWlzX2h5/YnJpZA",
+    name: "Andamios tubulares",
+    description: "Seguros y fáciles de montar para trabajos en altura.",
+    price: 5000,
+  },
+  {
+    id: 2,
+    image:
+      "https://imgs.search.brave.com/AG3GyTmVGGnPRl9GISNve0MXt8cUloGJpgL7P6cpGGw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cm9wZWxhdG8uY29t/LmFyLzE5ODM2LWhv/bWVfZGVmYXVsdC8x/MDYxMjk3X0hvcm1p/Z29uZXJhLTEzMEx0/cy1yZWZvcnphZGEt/LS0zNC1IUC1ydWVk/YS1tYWNpemEuanBn",
+    name: "Hormigonera",
+    description: "Ideal para mezclas de cemento en obras pequeñas y medianas.",
+    price: 7000,
+  },
+  {
+    id: 3,
+    image:
+      "https://imgs.search.brave.com/Ej_yzO3TL0xNX55-4WSskFimhe1X52iL8HkfLotI4Oo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/YXJjb21hcXVpbmFy/aWFzLmNvbS9pbWcv/cGhvdG8vQTUwMDA1/MDIzLmpwZw",
+    name: "Martillo demoledor Bosch 29kg",
+    description: "Máxima potencia para demoliciones exigentes.",
+    price: 12000,
+  },
+  {
+    id: 4,
+    image:
+      "https://imgs.search.brave.com/KI3hzD-lGKOfKmIQApk-RDWkVntORsQamFU6jwyc4O0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/Xzc2NTI5Ny1NTEE4/OTM0MDI4MDMyMV8w/ODIwMjUtRS53ZWJw",
+    name: "Rotopercutor Bosch 11kg",
+    description: "Perfecto para perforaciones pesadas en construcción.",
+    price: 9000,
+  },
+  {
+    id: 5,
+    image:
+      "https://imgs.search.brave.com/6QrzLF3wTN3IEsj5UO_qPy-mIKVs2iDkDF1hHP71Ktg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tbGEt/czEtcC5tbHN0YXRp/Yy5jb20vNzU1ODI2/LU1MQTkwNzM3NDAx/NTk3XzA4MjAyNS1G/LmpwZw",
+    name: "Cortacerco",
+    description: "Mantén tu jardín prolijo con facilidad.",
+    price: 4000,
+  },
+  {
+    id: 6,
+    image:
+      "https://imgs.search.brave.com/5lkm1LDzHkSj5ABmOt8B_xz5YQiQ-kVXm134BtaAxVE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/Xzc1NjM1OC1NTEE4/MjY0NjU4Mzc1Ml8w/MzIwMjUtRS53ZWJw",
+    name: 'Amoladora 7"',
+    description: "Potente herramienta para cortes y desbastes.",
+    price: 4500,
+  },
+  {
+    id: 7,
+    image:
+      "https://imgs.search.brave.com/1h8TJY-qR-CAEoeaEpdTJUnMLJlvB2zgpHMeB8etpUk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/XzgzMDk1OC1NTEE3/NDY3NzMwNTUzNV8w/MjIwMjQtVi53ZWJw",
+    name: "Generador 7.5 KVA",
+    description: "Energía confiable para obras y emergencias.",
+    price: 15000,
+  },
+  {
+    id: 8,
+    image:
+      "https://imgs.search.brave.com/N-8P36xM8jYuK7j-p7RsfoqMn983N139qfHsaz7PWu8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/Xzk0MDg5MC1NTEE5/MjQzNjcyMTE5NF8w/OTIwMjUtRS53ZWJw",
+    name: "Ingletadora",
+    description: "Cortes precisos para madera y perfiles.",
+    price: 8000,
+  },
+  {
+    id: 9,
+    image:
+      "https://imgs.search.brave.com/usks2QV91Sq51CAUBExKljwxf-mWNKYO_R9jgM11Kx8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/XzY1ODkxMS1NTFU3/ODA4MjUwMjYxNF8w/ODIwMjQtRS53ZWJw",
+    name: "Atornillador",
+    description: "Herramienta práctica para montaje y bricolaje.",
+    price: 3000,
+  },
+  {
+    id: 10,
+    image:
+      "https://imgs.search.brave.com/e1s76z03J5dqcjyjbQrhF6aeiWWkPViWhSdswhLOGFk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/XzYzODI4OC1NTEE4/MDE2ODQxNjkyNF8x/MTIwMjQtRS53ZWJw",
+    name: "Hidrolavadora",
+    description: "Limpieza a presión para superficies y vehículos.",
+    price: 6500,
+  },
+  {
+    id: 11,
+    image:
+      "https://imgs.search.brave.com/S6zTF1knDUShS70nXwSTXgSk0CYGAtPSoUtoQoss45c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/XzgyOTI0OS1NTEE0/MTg5MDIxMzE5MF8w/NTIwMjAtRS53ZWJw",
+    name: "Termofusora",
+    description: "Soldadura de caños plásticos rápida y segura.",
+    price: 3500,
+  },
+  {
+    id: 12,
+    image:
+      "https://imgs.search.brave.com/EgJAY42wZ-bmTecWRWKokhCUxBs7djm1BFnfmewBkhE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTcy/Nzg2NDIyL2VzL2Zv/dG8vZnVzZS1haXNs/YWRvLWRlLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1iWi1Y/QjhfNXVoVVltQVA0/NGh5N1JYbFBqelJS/WXZhOHQzYldZUjQ5/d1hzPQ",
+    name: "Motosierra",
+    description: "Ideal para cortar troncos y tareas forestales.",
+    price: 6500,
+  },
+  {
+    id: 13,
+    image:
+      "https://imgs.search.brave.com/HxjI9AZSCtX_SpnMtTorlaabhSyvgiW_y2Zxn-cFNiE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/XzY3NjQ2OC1NTEE4/MjU4Mjk2MTE1MF8w/MzIwMjUtRS53ZWJw",
+    name: "Carretilla",
+    description: "Transporte de materiales sencillo y práctico.",
+    price: 2000,
+  },
+  {
+    id: 14,
+    image:
+      "https://imgs.search.brave.com/9UlakYsWPTQoJnZgdMza0UUf0EYGzOdrLghgYwxqtIM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2VudHJvZGVtYXRl/cmlhbGVzLmNvbS5h/ci9pbWFnZXMyLzMw/MHgzMDAvMTkyOTg5/MDUyNi1lc2NhbGVy/YS1hbHVtaW5pby1l/eHRlbnNpYmxlLTE0/LWVzY2Fsb25lcy1w/b3ItdHJhbW8tMWI3/Yjc0NjIuanBn",
+    name: "Escalera",
+    description: "Herramienta esencial para trabajos en altura.",
+    price: 2000,
+  },
+];
