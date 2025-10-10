4af84d0bea16ac6c9ac180a714623d8366e6a3fa
nicoplacente <nicolasplacente@gmail.com>
2025-10-09 21:09:11 -0300

    create new cards styles

--- src/libs/reviews-constants.js
+++ src/libs/reviews-constants.js
@@ -1,85 +1,81 @@
 export const REVIEWS = [
   {
-    name: "Carlos Gómez",
+    name: "bruno fox",
     review:
-      "Muy buena atención y herramientas en excelente estado. Recomiendo 100%.",
+      "Tiene de todo pero te conviene llamar con anticipación si necesitas alguna herramienta que es muy buscada.",
     stars: 5,
   },
   {
-    name: "Laura Fernández",
-    review:
-      "Alquilé una hormigonera y funcionó perfecto. Servicio rápido y confiable.",
+    name: "Fran Ibañez",
+    review: "Siempre tienen todo. Algunas cosas un poco caras de alquilar.",
     stars: 5,
   },
   {
-    name: "Martín López",
+    name: "Ale Casadey",
     review:
-      "Gran variedad de herramientas, buenos precios y flexibilidad en el alquiler.",
+      "Excelentes maquinarias y servicio al cliente. Tuvimos un inconveniente con una máquina y respondieron inmediatamente para solucionarlo.",
+    stars: 5,
+  },
+  {
+    name: "Facundo Schwindt",
+    review:
+      "Excelente atención y gran variedad de herramientas y maquinarias!!",
+    stars: 5,
+  },
+  {
+    name: "Kike Bauer",
+    review:
+      "Unico comercio local dedicado al alquiler de todo tipo de herramientas.",
+    stars: 3,
+  },
+  {
+    name: "Fernando Tusek",
+    review: "Muy completo!!!",
     stars: 4,
   },
   {
-    name: "Sofía Martínez",
-    review:
-      "Me salvaron con una escalera a último momento, muy buena predisposición.",
+    name: "Ezequiel Duckwen",
+    review: "Buena atención!",
     stars: 5,
   },
   {
-    name: "Jorge Ramírez",
-    review:
-      "El taladro que alquilé funcionó bárbaro, aunque me costó un poco coordinar la entrega.",
-    stars: 4,
-  },
-  {
-    name: "Valentina Torres",
-    review: "Excelente servicio, puntuales y muy amables. Todo salió perfecto.",
+    name: "Mariano Gonzalez",
+    review: "La expocicion de productos.",
     stars: 5,
   },
   {
-    name: "Andrés Castillo",
-    review:
-      "Buenos precios y herramientas en muy buen estado. Volvería a alquilar sin dudarlo.",
+    name: "TONY MOTO",
+    review: "Buenos precios.",
     stars: 5,
   },
-  {
-    name: "Mariana Ruiz",
-    review:
-      "El generador funcionó muy bien, aunque hubiera preferido más opciones de pago.",
-    stars: 4,
-  },
-  {
-    name: "Federico Ponce",
-    review:
-      "Alquilé varias herramientas para una obra pequeña y me simplificaron todo. Excelente experiencia.",
-    stars: 5,
-  },
-  {
-    name: "Gabriela Álvarez",
-    review:
-      "Muy buen trato y asesoramiento, me recomendaron justo lo que necesitaba.",
-    stars: 5,
-  },
-  {
-    name: "Natalia Herrera",
-    review:
-      "Todo impecable, desde la reserva hasta la devolución. ¡Muy recomendable!",
-    stars: 5,
-  },
-  {
-    name: "Pablo Medina",
-    review:
-      "El servicio es bueno, aunque me hubiera gustado que tengan más variedad de herramientas eléctricas.",
-    stars: 4,
-  },
-  {
-    name: "Camila López",
-    review:
-      "Super profesionales, me explicaron cómo usar la herramienta antes de retirarla.",
-    stars: 5,
-  },
-  {
-    name: "Hernán Sánchez",
-    review:
-      "Precios accesibles y buen servicio, aunque la amoladora estaba un poco desgastada.",
-    stars: 4,
-  },
+  // {
+  //   name: "Gabriela Álvarez",
+  //   review:
+  //     "Muy buen trato y asesoramiento, me recomendaron justo lo que necesitaba.",
+  //   stars: 5,
+  // },
+  // {
+  //   name: "Natalia Herrera",
+  //   review:
+  //     "Todo impecable, desde la reserva hasta la devolución. ¡Muy recomendable!",
+  //   stars: 5,
+  // },
+  // {
+  //   name: "Pablo Medina",
+  //   review:
+  //     "El servicio es bueno, aunque me hubiera gustado que tengan más variedad de herramientas eléctricas.",
+  //   stars: 4,
+  // },
+  // {
+  //   name: "Camila López",
+  //   review:
+  //     "Super profesionales, me explicaron cómo usar la herramienta antes de retirarla.",
+  //   stars: 5,
+  // },
+  // {
+  //   name: "Hernán Sánchez",
+  //   review:
+  //     "Precios accesibles y buen servicio, aunque la amoladora estaba un poco desgastada.",
+  //   stars: 4,
+  // },
 ];
